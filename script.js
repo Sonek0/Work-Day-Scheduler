2a46b0dc7b25cd0f254aee01c3eea5b49f16017f
jacob <jacob.7050@gmail.com>
2023-01-25 22:39:46 -0800

    Fixed illegal () line 45:53 error

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ $('.saveBtn').on('click', function() {
 // adding a hour function with regular hours along with a loop if time has been moved
 function hourUpdater() {
   var currentHour = dayjs().hour();
-  $('time-block').each(function () {
+  $('.time-block').each(function () {
     var blockHour = parseInt($(this).attr('id').split('-')[1]);
 
     if(blockHour < currentHour) {
@@ -42,15 +42,15 @@ hourUpdater();
 setInterval(hourUpdater, 15000);
 
 //storage load from saved data
-$('#hour-9 .description').val(localStorage.getItem)('hour-9');
-$('#hour-10 .description').val(localStorage.getItem)('hour-10');
-$('#hour-11 .description').val(localStorage.getItem)('hour-11');
-$('#hour-12 .description').val(localStorage.getItem)('hour-12');
-$('#hour-13 .description').val(localStorage.getItem)('hour-13');
-$('#hour-14 .description').val(localStorage.getItem)('hour-14');
-$('#hour-15 .description').val(localStorage.getItem)('hour-15');
-$('#hour-16 .description').val(localStorage.getItem)('hour-16');
-$('#hour-17 .description').val(localStorage.getItem)('hour-17');
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
 $('#currentDay').text(dayjs("03-14-2023").format("dddd, MMMM D, YYYY"));
 
